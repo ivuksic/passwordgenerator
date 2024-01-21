3ed3265b7e84f3df16ea6173f885598bf5e336f1
Ivan Vuksic <ivuksic@gmail.com>
2024-01-21 19:14:49 +0100

    genearete password function

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const passwordBox = document.getElementById("password");
-const lenght = 12;
+const length = 12;
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
-const symbols = "!@#$%^&*()_+-=[];',./{}:|<>?</>";
+const symbol = "!@#$%^&*()_+-=[];',./{}:|<>?</>";
+
+const allChars = upperCase + lowerCase + number + symbol;
 
 function createPassword() {
   let password = "";
   password += upperCase[Math.floor(Math.random() * upperCase.length)];
-  password += lowerCaseCase[Math.floor(Math.random() * lowerCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
   password += number[Math.floor(Math.random() * number.length)];
-  password += symbols[Math.floor(Math.random() * symbols.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
 }
