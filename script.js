8f86d6ca3d96c04da38a7bbaf1abea7bb23525c2
Ivan Vuksic <ivuksic@gmail.com>
2024-01-21 19:07:11 +0100

    connected js added const

---
+++ script.js
@@ -0,0 +1,15 @@
+const passwordBox = document.getElementById("password");
+const lenght = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbols = "!@#$%^&*()_+-=[];',./{}:|<>?</>";
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCaseCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
+}
